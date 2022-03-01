7072293f6e09d013deddbff0f50725230ad9f21e
Meryl M <73620120+hypatiav2@users.noreply.github.com>
2022-02-28 23:05:54 -0800

    Add files via upload

---
+++ maincapture.py
@@ -0,0 +1,56 @@
+#complete CAPITALIZED sections
+
+#AUTHOR: 
+#DATE:
+
+#import libraries
+import time
+import os
+import board
+import busio
+import adafruit_bno055
+#from git import Repo
+from picamera import PiCamera
+
+#setup imu and camera
+i2c = busio.I2C(board.SCL, board.SDA)
+sensor = adafruit_bno055.BNO055_I2C(i2c)
+camera = PiCamera()
+
+"""
+#bonus: function for uploading image to Github
+def git_push():
+    try:
+        repo = Repo('/home/pi/FlatSatChallenge') #PATH TO YOUR GITHUB REPO
+        repo.git.add('folder path') #PATH TO YOUR IMAGES FOLDER WITHIN YOUR GITHUB REPO
+        repo.index.commit('New Photo')
+        print('made the commit')
+        origin = repo.remote('origin')
+        print('added remote')
+        origin.push()
+        print('pushed changes')
+    except:
+        print('Couldn\'t upload to git')
+"""
+
+    
+#SET THRESHOLD
+threshold = 0
+
+
+#read acceleration
+while True:
+    accelX, accelY, accelZ = sensor.acceleration
+
+    if accelX > threshold:
+        #PAUSE
+
+    
+        #TAKE/SAVE/UPLOAD A PICTURE 
+        name = "n"     #Last Name, First Initial  ex. FoxJ
+        
+        if name:
+            t = time.strftime("_%H%M%S")      # current time string
+            imgname = ('/home/pi/FlatSatChallenge/Images/YOURFOLDER/%s%s' % (name,t)) #change directory to your folder
+    
+            #<YOUR CODE GOES HERE>#
