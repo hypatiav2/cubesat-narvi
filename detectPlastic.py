ed80984bf4f3013cb6cd8ea4d9924b1cef21efd7
Meryl M <73620120+hypatiav2@users.noreply.github.com>
2022-05-20 23:06:40 -0700

    update plastic recognition

--- detectPlastic.py
+++ detectPlastic.py
@@ -1,16 +1,22 @@
 import cv2
+import numpy as np
 
-# Load image, grayscale, Gaussian blur, Otsu's threshold
-image = cv2.imread("1.jpg")
+image = cv2.imread('2.jpg')
+original = image.copy()
+
 gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-blur = cv2.GaussianBlur(gray, (5,5), 0)
-thresh = cv2.threshold(blur, 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)[1]
+blur = cv2.GaussianBlur(gray, (5,5), 1)
+thresh = cv2.adaptiveThreshold(blur,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C,cv2.THRESH_BINARY_INV,11,2)
 
-# Find bounding box
-x,y,w,h = cv2.boundingRect(thresh)
-cv2.rectangle(image, (x, y), (x + w, y + h), (36,255,12), 2)
-cv2.putText(image, "w={},h={}".format(w,h), (x,y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (36,255,12), 2)
+ROI_number = 0
+cnts = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+cnts = cnts[0] if len(cnts) == 2 else cnts[1]
+for c in cnts:
+    x,y,w,h = cv2.boundingRect(c)
+    if w > 40 and h > 40:
+        cv2.rectangle(image, (x, y), (x + w, y + h), (0,0,255), 2)
 
-cv2.imshow("thresh", thresh)
-cv2.imshow("image", image)
+cv2.imshow('image', image)
+cv2.imshow('blur', blur)
+cv2.imshow('Thresh',thresh)
 cv2.waitKey()
